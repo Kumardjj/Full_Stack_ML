01e5b9a0f455fadfbc7df4908c0093113f26e109
Dheeraj Kumar <kumardjj2001@gmail.com>
2026-05-06 15:55:02 +0530

    Selection sort , beginning of DSA

---
+++ DSA/sort.py
@@ -0,0 +1,10 @@
+# Selection sort :- it is a simple sorting algorithm. It works by repeatedly selecting the smallest (or largest) element from the unsorted portion of the list and swapping it with the first unsorted element until the entire list is sorted.
+lst = [3,4,1,2,6]
+n=len(lst)
+for i in range(0,n-1):
+    min_index=i
+    for j in range(i+1,n):
+        if(lst[j]<lst[min_index]):
+            min_index=j
+    lst[i],lst[min_index]=lst[min_index],lst[i]
+print(lst)

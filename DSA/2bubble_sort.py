a4e4bdc773725c17241f03bc063737ce454ce8e5
Dheeraj Kumar <kumardjj2001@gmail.com>
2026-05-06 16:56:06 +0530

    add a break condition for beeter performance

--- DSA/2bubble_sort.py
+++ DSA/2bubble_sort.py
@@ -1,10 +1,14 @@
 #bubble sort :- it is a simple sorting algorithm that repeatedly steps through the list, compares adjacent elements and swaps them if they are in the wrong order. The pass through the list is repeated until the list is sorted.
-lst = [7,4,8,5,3]
+lst = [3,4,5,6,6]
 n=len(lst)
 for i in range(0,n-1):
+    check=False
     for j in range(0, n-i-1):
         if(lst[j+1]<lst[j]):
             lst[j],lst[j+1]=lst[j+1],lst[j]
+            check=True
+    if(check == False):
+        break       
 print(lst)
 
 # time complexity :- the time complexity of bubble sort is O(n^2) in the worst and average cases, and O(n) in the best case when the list is already sorted. This is because in the worst and average cases, the algorithm needs to compare each element with every other element, resulting in n*(n-1)/2 comparisons. In the best case, the algorithm only needs to make one pass through the list to confirm that it is already sorted.

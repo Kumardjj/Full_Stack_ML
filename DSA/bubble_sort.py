a6226b9d2e74a78950de8e614d590388a9ca69da
Dheeraj Kumar <kumardjj2001@gmail.com>
2026-05-06 16:46:42 +0530

    space complexity of bubble sort

--- DSA/bubble_sort.py
+++ DSA/bubble_sort.py
@@ -6,3 +6,7 @@ for i in range(0,n-1):
         if(lst[j+1]<lst[j]):
             lst[j],lst[j+1]=lst[j+1],lst[j]
 print(lst)
+
+# time complexity :- the time complexity of bubble sort is O(n^2) in the worst and average cases, and O(n) in the best case when the list is already sorted. This is because in the worst and average cases, the algorithm needs to compare each element with every other element, resulting in n*(n-1)/2 comparisons. In the best case, the algorithm only needs to make one pass through the list to confirm that it is already sorted.
+
+# space complexity :- the space complexity of bubble sort is O(1) because it only requires a constant amount of additional space to perform the sorting. The algorithm sorts the list in place, meaning it does not require any additional data structures to hold the sorted elements. Therefore, the space complexity is constant.and it is basically the auxiliary space complexity that we are talking about here, which is O(1) for bubble sort.

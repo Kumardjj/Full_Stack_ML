6e2a8f0a68658b619a3871d961135e581bdf4621
Dheeraj Kumar <kumardjj2001@gmail.com>
2026-05-15 12:45:39 +0530

    12

--- DSA/12search_insert.py
+++ DSA/12search_insert.py
@@ -1,20 +1,21 @@
 def search_insert(arr,target):
-    n=len(arr)-1
+    n=len(arr)
     start = 0
     end= n-1
-    result = n+1
+    result = n
     while(start<=end):
         mid = start +((end-start)//2)
         if(arr[mid] == target):
-            return mid
-        elif(arr[mid]>target):
-            end = mid - 1
             result = mid
+            break
+        elif(arr[mid]>target):
+            result = mid
+            end = mid - 1   
         else:
             start = mid + 1
     return result
 
 
 arr= [1,4,6,8,10,14,16,18]
-target = 17
+target = 170
 print(search_insert(arr,target)) 
